b7ffadcc0e2493528c060f6e1c9239f595d29f08
gknows1234 <gknows12345@gmail.com>
2026-07-09 14:58:13 +0530

    Added Week 3 exercises - Spring Boot REST and JWT authentication

---
+++ Week-3/spring-learn/src/main/java/com/cognizant/springlearn/SpringLearnApplication.java
@@ -0,0 +1,31 @@
+package com.cognizant.springlearn;
+
+import java.text.SimpleDateFormat;
+import java.text.ParseException;
+import java.util.Date;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+@SpringBootApplication
+public class SpringLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLearnApplication.class, args);
+		System.out.println("Inside main");
+		displayDate();
+	}
+
+	public static void displayDate() {
+		ApplicationContext context = new ClassPathXmlApplicationContext("date-format.xml");
+		SimpleDateFormat format = context.getBean("dateFormat", SimpleDateFormat.class);
+		try {
+			Date date = format.parse("31/12/2018");
+			System.out.println("Parsed Date: " + date);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ Week-3/spring-learn/src/main/java/com/cognizant/springlearn/controller/EmployeeController.java
@@ -0,0 +1,21 @@
+package com.cognizant.springlearn.controller;
+
+import com.cognizant.springlearn.model.Employee;
+import com.cognizant.springlearn.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+@RestController
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping("/employees")
+    public ArrayList<Employee> getAllEmployees() {
+        return employeeService.getAllEmployees();
+    }
+}
